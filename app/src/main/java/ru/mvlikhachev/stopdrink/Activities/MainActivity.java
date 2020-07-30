9615895b479cbdde7954b2c9b46a2a1fc27dd36b
Maksim Likhachev <wwwqwwwqwwwqwwwq@gmail.com>
2020-07-30 09:57:42 +0300

    Create app, add dependencies(Firebase + material), create splash markup

---
+++ app/src/main/java/ru/mvlikhachev/stopdrink/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.mvlikhachev.stopdrink.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.mvlikhachev.stopdrink.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/ru/mvlikhachev/stopdrink/Activities/SplashActivity.java
@@ -0,0 +1,40 @@
+package ru.mvlikhachev.stopdrink.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.mvlikhachev.stopdrink.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(
+                            SplashActivity.this,
+                            LoginSugnUpActivity.class
+                    ));
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
