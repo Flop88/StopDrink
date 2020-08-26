59351f00941388f0e4a06c97bd70018a24737eed
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-08-26 11:39:19 +0300

    DEVELOP: Implement load profile image from FirebaseDatabase Storage

--- app/src/main/java/ru/mvlikhachev/stopdrink/Activities/ProfileActivity.kt
+++ app/src/main/java/ru/mvlikhachev/stopdrink/Activities/ProfileActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.ktx.Firebase
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
 import ru.mvlikhachev.stopdrink.R
 import ru.mvlikhachev.stopdrink.Utils.Utils
@@ -45,6 +48,17 @@ class ProfileActivity : AppCompatActivity() {
                 "128")
         val profileImageFromDb = sharedPreferences.getString(APP_PREFERENCES_KEY_PROFILE_IMAGE,
                 "128")
+        // Reference to an image file in Cloud Storage
+        val storageReference = profileImageFromDb;
+
+// ImageView in your Activity
+        val imageView = findViewById<ImageView>(R.id.profileAvatarImageView)
+
+// Download directly from StorageReference using Glide
+// (See MyAppGlideModule for Loader registration)
+        Glide.with(this /* context */)
+                .load(storageReference)
+                .into(imageView)
 
         val date = Utils.calculateTimeWithoutDrink(oldDate)
         val daysWithoutDrink = date[0]
