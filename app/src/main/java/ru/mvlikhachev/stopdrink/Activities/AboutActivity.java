33e602522d5c107e826afece5370601d157693ad
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-09-01 22:57:50 +0300

    DEVELOP: Add social links in about acttivity

--- app/src/main/java/ru/mvlikhachev/stopdrink/Activities/AboutActivity.java
+++ app/src/main/java/ru/mvlikhachev/stopdrink/Activities/AboutActivity.java
@@ -1,8 +1,11 @@
 package ru.mvlikhachev.stopdrink.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,13 +22,35 @@ import static ru.mvlikhachev.stopdrink.Utils.NotificationReceiver.showRatingUser
 
 public class AboutActivity extends AppCompatActivity {
 
+    private ImageView vk;
+    private ImageView insta;
+    private ImageView telega;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        vk = findViewById(R.id.vkImageView);
+        insta = findViewById(R.id.instagramImageView);
+        telega = findViewById(R.id.telegramImageView);
+
         // Убрать ActionBar
         getSupportActionBar().hide();
+
+        vk.setOnClickListener(view -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/mv.likhachev"));
+            startActivity(browserIntent);
+        });
+
+        insta.setOnClickListener(view -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/mv.likhachev"));
+            startActivity(browserIntent);
+        });
+        telega.setOnClickListener(view -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/mlikhachev"));
+            startActivity(browserIntent);
+        });
     }
 
     public void writeReview(View view) {
