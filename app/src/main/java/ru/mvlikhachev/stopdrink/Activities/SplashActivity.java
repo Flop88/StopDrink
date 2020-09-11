90b3778c82b2f13f0f13211329505d4acb9ce31d
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-09-11 10:54:32 +0300

    add in-app dependency

--- app/src/main/java/ru/mvlikhachev/stopdrink/Activities/SplashActivity.java
+++ app/src/main/java/ru/mvlikhachev/stopdrink/Activities/SplashActivity.java
@@ -5,14 +5,25 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 import ru.mvlikhachev.stopdrink.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        Bundle bundle = new Bundle();
+        String method = "METHOD";
+        bundle.putString(FirebaseAnalytics.Param.METHOD,method);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, bundle);
 
         // Убрать ActionBar
         getSupportActionBar().hide();
