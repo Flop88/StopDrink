5a74398d11568c4be84765541d802d99ec27d28b
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-27 11:11:37 +0300

    Load time from DB in mainactivity.

--- app/src/main/java/ru/mvlikhachev/stopdrink/view/MainActivity.java
+++ app/src/main/java/ru/mvlikhachev/stopdrink/view/MainActivity.java
@@ -146,21 +146,17 @@ public class MainActivity extends AppCompatActivity {
 
         goOnlineConnectiontoDatabase();
         if (Utils.hasConnection(this)) {
-            // load name from firebase database
-//            getNameFromDatabase();
+
             loadData(firebaseUser.getUid());
 
             // load last date when user drink alcohol from firebase database
-            Thread updateDateThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while(true){
-                        try {
-                            getDateOfLastDrinkFromDatabase();
-                            Thread.sleep(60000); //1000 - 1 сек
-                        } catch (InterruptedException ex) {
-                            ex.printStackTrace();
-                        }
+            Thread updateDateThread = new Thread(() -> {
+                while(true){
+                    try {
+                        setTime();
+                        Thread.sleep(60000); //1000 - 1 сек
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
                     }
                 }
             });
@@ -189,9 +185,26 @@ public class MainActivity extends AppCompatActivity {
     private void loadData(String uid) {
         mainActivityViewModel.getUser(uid).observe(this, user -> {
             Log.d("TAGUSER", "Current name: " + user.getName());
+            Log.d("TAGUSER", "Current name: " + user.getDateWhenStopDrink());
             helloUsernameTextView.setText("Здраствуйте, " + user.getName());
+            lastDrinkDate = user.getDateWhenStopDrink();
+            Log.d("DrinkDate", "Last in loadData: " + lastDrinkDate);
+
+            setTime();
 
         });
+    }
+
+    private void setTime() {
+        Log.d("DrinkDate", "Last in setTime: " + lastDrinkDate);
+        String[] dates = Utils.calculateTimeWithoutDrink(lastDrinkDate);
+        daysWithoutDrink = dates[0];
+        setNotDrinkTime(dates[0],dates[1],dates[2]);
+
+        // Show notification if hour = 00 and minute = 00
+        if (dates[1].equals("00") && dates[2].equals("00")) {
+            Utils.showNotificationWithDate(getApplicationContext(),dates[0]);
+        }
     }
 
     // Show bottom navighation menu
@@ -291,53 +304,6 @@ public class MainActivity extends AppCompatActivity {
         userDatabaseReference.addChildEventListener(loadDateUserChildeEventListener);
     }
 
-
-    // Get name from firebase database method
-    private void getNameFromDatabase() {
-        userChildeEventListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                User user = snapshot.getValue(User.class);
-                if (user.getId().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
-                    username = user.getName();
-                    helloUsernameTextView.setText("Здраствуйте, " + username);
-
-                    String about = user.getAboutMe();
-                    String url = user.getProfileImage();
-
-                    // Save "username" on local storage
-                    editor.putString(APP_PREFERENCES_KEY_NAME, username);
-                    editor.putString(APP_PREFERENCES_KEY_PROFILE_IMAGE, url);
-                    editor.putString(APP_PREFERENCES_KEY_ABOUT_ME, about);
-                    editor.apply();
-                }
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        };
-
-        userDatabaseReference.addChildEventListener(userChildeEventListener);
-    }
-
-
     // Set date data in TextView
     private void setNotDrinkTime(String days, String hours, String minutes) {
         daysTextView.setText(days + " дней");
@@ -396,7 +362,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         mAdView.resume();
 
-        getDateOfLastDrinkFromDatabase();
+        setTime();
         goOnlineConnectiontoDatabase();
 
         if (mInterstitialAd.isLoaded()) {
